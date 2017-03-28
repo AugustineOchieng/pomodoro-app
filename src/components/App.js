3bc5c9aa1cd3691b1d9dfed692dac4e4210b7094
Sam Atkins <sam@cubi.io>
2017-03-28 16:00:38 +0100

    feat: Add footer

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Header from './Header';
 import Pomodoro from './Pomodoro';
+import Footer from './Footer';
 
 class App extends Component {
   render() {
@@ -8,6 +9,7 @@ class App extends Component {
       <div className="wrapper">
         <Header />
         <Pomodoro />
+        <Footer />
       </div>
     );
   }
---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class Footer extends React.Component {
+ 
+  render() {
+    return (
+      <div className="footer">Created with &hearts; by <a href="https://samatkins.me/" target="_blank">Sam</a>. View source code on <a href="https://github.com/cubiio/pomodoro-app" target="_blank">GitHub</a>.</div>
+    )
+  }
+}
+
+export default Footer;
