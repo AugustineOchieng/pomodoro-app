f5b9970aa16c7f1b21fcffeccb5fa13236a48922
Sam Atkins <sam@cubi.io>
2017-03-28 09:05:11 +0100

    feat: Render block with user's local time

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import Header from './Header';
 import Timer from './Timer';
+import Clock from './Clock';
 
 class App extends Component {
   render() {
     return (
       <div className="wrapper">
         <Header />
+        <Clock />
         <Timer />
       </div>
     );
